b6e0c6748de3e27309c0e3f5853c8fb27af1ef5d
leonidyuce <hllsmtyc@gmail.com>
2024-01-22 06:56:55 +0100

    22.1.24

--- mergesortedarray.py
+++ mergesortedarray.py
@@ -1,6 +1,9 @@
 # 21.1.24
-class Solution(object):
+class Solution:
     def merge(self, nums1, m, nums2, n):
         nums1[m:] = nums2
         nums1.sort()
         print(nums1)
+
+
+Solution().merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
---
+++ removeelement.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        print(nums)
+
+
+Solution().removeElement([3, 2, 2, 3], 3)
+
+
+# In computer science, an in-place algorithm is an algorithm that operates directly on the input data structure without requiring extra space proportional to the input size. In other words, it modifies the input in place, without creating a separate copy of the data structure. An algorithm which is not in-place is sometimes called not-in-place or out-of-place.
